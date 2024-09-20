d5e5b9c884d93a5da5752a14a01a0f19dc5b9893
LordOdin <derekrb25017@gmail.com>
2024-09-20 09:38:33 -0400

    Update file reading and writing

--- manifest.py
+++ manifest.py
@@ -4,6 +4,10 @@ version = sys.argv[sys.argv.index("--version") + 1]
 version = version.split("/")[-1] if "/" in version else version
 
 addon_directory = "/tmp/OctaRender"
+extension_index = os.path.join(addon_directory, "extensions_index.json")
+blender_manifest = os.path.join(addon_directory, "blender_manifest.toml")
+zip_path = f"{addon_directory}.zip"
+
 exclude_files = ["__pycache__",
                  ".git",
                  ".github",
@@ -15,25 +19,32 @@ exclude_files = ["__pycache__",
                  "manifest.py",                 
                  "update_manifest.py"]
 
-with open(os.path.join(addon_directory, "blender_manifest.toml"), "r+") as f:
+
+with open(blender_manifest, "r") as f:
     manifest = toml.loads(f.read())
+
+with open(blender_manifest, "w") as f:
     manifest['version'] = version
     f.write(toml.dumps(manifest))
 
-with zipfile.ZipFile(f"{addon_directory}.zip", "w") as zip_archive:
+
+with zipfile.ZipFile(zip_path, "w") as zip_archive:
     for root, dirs, files in os.walk(addon_directory):
         for file in files:
             file_path = os.path.join(root, file)
             if any(excluded_file in file_path for excluded_file in exclude_files):
                 continue
             else:
-                zip_archive.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), '/tmp/'))
+                zip_archive.write(file_path, os.path.relpath(file_path, '/tmp/'))
 
-with open(f"{addon_directory}.zip", "rb") as archive:
-    archive_content = archive.read()
 
-with open(os.path.join(addon_directory, "extensions_index.json"), "r+") as f:
+with open(zip_path, "rb") as f:
+    archive_content = f.read()
+
+with open(extension_index, "r") as f:
     index = json.loads(f.read())
+
+with open(extension_index, "w") as f:
     index['data'][0]['version'] = version
     index['data'][0]['archive_url'] = f"https://github.com/octaspace/blender-addon/releases/download/{version}/OctaRender.zip"
     index['data'][0]['archive_size'] = len(archive_content)
