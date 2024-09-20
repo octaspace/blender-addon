1da24ca6db5cbdb4f17add2676ed1d269e2f9d38
LordOdin <derekrb25017@gmail.com>
2024-09-20 08:37:07 -0400

    Add extensions json

--- manifest.py
+++ manifest.py
@@ -1,17 +1,20 @@
-import toml, sys
+import toml, sys, json, hashlib
 
 version = sys.argv[sys.argv.index("--version") + 1]
+version = version.split("/")[-1] if "/" in version else version
 
-if "/" in version:
-    version = version.split("/")[-1]
+with open("OctaRender.zip", "rb") as archive:
+    archive_content = archive.read()
 
-print("Updating TOML to version", version)
+with open("blender_manifest.toml", "r+") as f:
+    manifest = toml.load(f)
+    manifest['version'] = version
+    toml.dump(manifest, f)
 
-
-with open("blender_manifest.toml", "r") as f:
-    data = toml.load(f)
-
-
-with open("blender_manifest.toml", "w") as f:
-    data['version'] = version
-    toml.dump(data, f)
+with open("blender_index.json", "r+") as f:
+    index = json.load(f)
+    index['data'][0]['version'] = version
+    index['data'][0]['archive_url'] = f"https://github.com/octaspace/blender-addon/releases/download/{version}/OctaRender.zip"
+    index['data'][0]['archive_size'] = len(archive_content)
+    index['data'][0]['archive_hash'] = f"sha256:{hashlib.sha256(archive_content).hexdigest()}"
+    json.dump(index, f, indent=4)
