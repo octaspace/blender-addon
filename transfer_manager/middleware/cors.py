9707d85322c411e0732a77b16c437eb0ee3521af
Frederik Gelder <frederik.gelder@freenet.de>
2024-10-18 00:22:08 +0200

    allow all origins explicitly

--- transfer_manager/middleware/cors.py
+++ transfer_manager/middleware/cors.py
@@ -1,5 +1,7 @@
 async def cors(request, response):
-    response.headers["Access-Control-Allow-Origin"] = "*"
+    origin = request.headers.get('Origin', None)
+    if origin is not None:
+        response.headers["Access-Control-Allow-Origin"] = origin
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
     response.headers["Access-Control-Allow-Headers"] = "*"
     response.headers["Access-Control-Max-Age"] = "300"
